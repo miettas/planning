54464469afb43e2ba7bd3c0cfea2c632e22feb1d
tony <tony@miettas.com.au>
2025-11-09 17:55:22 +1100

    trevor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
         <!-- Scripts -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        @vite(['resources/sass/app.scss', 'resources/sass/knox.scss', 'resources/js/app.js'])
     </head>
     <body>
         <div class="w-screen">
